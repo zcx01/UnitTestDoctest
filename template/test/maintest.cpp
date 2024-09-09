689d4e4f91f0b5e3810c89491bcae120ae1f5f6d
chengxiong.zhu <chengxiong.zhu@megatronix.co>
2024-09-09 17:52:59 +0800

    模板

--- template/test/maintest.cpp
+++ template/test/maintest.cpp
@@ -1,8 +1,10 @@
 #include "maintest.hpp"
 #include "icdoctest.h"
-#include "project/fds_can_main_entry.hpp"
+#include "can_api.h"
+#include "fds_can_main_entry.hpp"
 
 void test_main()
 {
     init_signal_process();
+    IC_SINGLE_SET_UINT32_T(CANSIG_GW_1F3__MdcNicaReminderAcoustic_g,1);
 }
