8d3170959c68e829418b55dcf809f02ff1d50dbc
chengxiong.zhu <chengxiong.zhu@megatronix.co>
2024-09-12 16:14:27 +0800

    添加执行权限

--- updatetestcode.sh
+++ updatetestcode.sh
@@ -11,6 +11,10 @@ update_files_and_dirs() {
     cp -rf $1/$2 ./
 }
 
+gitDir=.git
+if [ ! -d $gitDir ]; then
+    rm -rf $gitDir
+fi
 
 update_files_and_dirs $testcode ./common
 update_files_and_dirs $testcode ./template
@@ -20,3 +24,5 @@ update_files_and_dirs $testcode ./InitProject.py
 update_files_and_dirs $testcode ./InitProject.sh
 update_files_and_dirs $testcode ./main.cpp
 update_files_and_dirs $testcode ./updatetestcode.sh
+cp -rf ./template/* ./
+rm -rf ./template
