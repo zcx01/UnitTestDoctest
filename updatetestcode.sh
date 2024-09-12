fa7a6c1eead5234229b461712c2b3802602623dc
chengxiong.zhu <chengxiong.zhu@megatronix.co>
2024-09-12 15:36:45 +0800

    添加更新测试模板的脚本

--- updatetestcode.sh
+++ updatetestcode.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/bash
+testcode=~/UnitTestDoctest/
+if [ $# -ne 1 ]; then
+     echo "use default path: $testcode"
+else
+    testcode=$1
+fi
 
 update_files_and_dirs() {
     rm -rf $2
@@ -6,10 +12,10 @@ update_files_and_dirs() {
 }
 
 
-update_files_and_dirs $1 ./common
-update_files_and_dirs $1 ./template
-update_files_and_dirs $1 ./build.sh
-update_files_and_dirs $1 ./CMakeLists.txt
-update_files_and_dirs $1 ./InitProject.py
-update_files_and_dirs $1 ./main.cpp
-update_files_and_dirs $1 ./updatetestcode.sh
+update_files_and_dirs $testcode ./common
+update_files_and_dirs $testcode ./template
+update_files_and_dirs $testcode ./build.sh
+update_files_and_dirs $testcode ./CMakeLists.txt
+update_files_and_dirs $testcode ./InitProject.py
+update_files_and_dirs $testcode ./main.cpp
+update_files_and_dirs $testcode ./updatetestcode.sh
